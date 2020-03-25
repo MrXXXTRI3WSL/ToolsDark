358da866c007471fe6177f1a73606ea4c3dfbfb1
MrXXXTRI3WSL <62236907+MrXXXTRI3WSL@users.noreply.github.com>
2020-03-26 04:21:16 +0700

    Add files via upload

---
+++ ToolsDark.py
@@ -0,0 +1,59 @@
+import time
+import subprocess
+import os
+import sys
+
+subprocess.call('clear',shell=True)
+subprocess.call('figlet Hack Fb',shell=True)
+time.sleep(3)
+
+print ("============================================")
+print (" Author : Mr XXXTRI3WSL")
+print (" Youtube : AGUNG ZKYT")
+print (" Thanks : Mr.D€X§T£R Mr Dhava")
+print (" GitHub : https://github.com/mrxxxtri3wsl")
+print ("============================================")
+print ("[+] PILIH MENU [+]")
+print ("[1] Dark Fbv1.7")
+print ("[2] Dark Fbv1.8")
+print ("[3] Dark Fb Premium")
+print ("[4] Dark Fbv2.0")
+print ("[5] Install bahan")
+time.sleep(3)
+pilih = str(input('[?] pilih : '))
+
+if pilih == "1":
+        os.system('git clone https://github.com/LOoLzeC/reverse_engineering')
+        print ("[+] penginstallan selesai")
+        print ("[+] selanjutnya ketik $ cd reverse_engineering")
+        print ("[+] lalu ketik $ python2 dark-memek.py ")
+elif pilih == "2":
+        os.system('git clone https://github.com/V4N654T/dark-fb')
+        print ("[+] penginstallan selesai")
+        print ("[+] selanjutnya ketik $ cd dark-fb")
+        print ("[+] lalu ketik $ python2 da.py")
+elif pilih == "3":
+        os.system('git clone https://github.com/TheMagizz/DarkPremium')
+        print ("[+] penginstallan selesai")
+        print ("[+] selanjutnya ketik $ cd DarkPremium")
+        print ("[+] lalu ketik $ python2 DarkFB.py")
+elif pilih == "4":
+        os.system('git clone https://github.com/rezadkim/dark-fb')
+        print ("[+] penginstallan selesai")
+        print ("[+] selanjutnya ketik $ cd dark-fb ")
+        print ("[+] lalu ketik $ python2 dark.py")
+elif pilih == "5":
+        os.system('apt update && apt upgrade')
+        os.system('pkg install python2')
+        os.system('pkg install figlet')
+        os.system('pkg install nano')
+        os.system('pkg install git')
+        print ("[+] penginstallan selesai"-fb)
+        print ("[+] lalu ketik $ python2 dark.py")
+elif pilih == "5":
+        os.system('apt update && apt upgrade')
+        os.system('pkg install python2')
+        os.system('pkg install figlet')
+        os.system('pkg install nano')
+        os.system('pkg install git')
+        print ("[+] penginstallan selesai")
